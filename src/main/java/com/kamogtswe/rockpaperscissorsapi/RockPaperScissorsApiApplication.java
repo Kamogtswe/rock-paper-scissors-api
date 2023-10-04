0657a1785123ae7d5a1e7fbda511db5ad662d840
Kamogetswe Machaka <machakathato@gmail.com>
2023-10-04 12:30:25 +0200

    initial commit

---
+++ src/main/java/com/kamogtswe/rockpaperscissorsapi/RockPaperScissorsApiApplication.java
@@ -0,0 +1,13 @@
+package com.kamogtswe.rockpaperscissorsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsApiApplication.class, args);
+	}
+
+}
