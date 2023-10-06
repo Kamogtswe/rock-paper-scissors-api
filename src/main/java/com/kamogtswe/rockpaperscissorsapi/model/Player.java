009c6265edd060a53f3120e856c8256318ea75bb
Kamogetswe Machaka <machakathato@gmail.com>
2023-10-06 12:59:23 +0200

    feat: Implement the Player class for game management
    - Added a class 'Player' to represent individual players in the game.
    - Implemented methods for setting player hands, tracking wins, and checking hand choices.

---
+++ src/main/java/com/kamogtswe/rockpaperscissorsapi/model/Player.java
@@ -0,0 +1,54 @@
+package com.kamogtswe.rockpaperscissorsapi.model;
+
+import java.util.List;
+import java.util.Objects;
+
+import com.kamogtswe.rockpaperscissorsapi.Game;
+import com.kamogtswe.rockpaperscissorsapi.model.hands.Hand;
+
+public class Player {
+    
+    private Game currentGame;
+    private Hand hand;
+    private int wins = 0;
+
+    public void join(Game game) {
+        this.currentGame = game;
+        this.currentGame.addPlayers(this);
+    }
+
+    public void choose(Hand hand) {
+        this.hand = hand;
+    }
+
+    public List<Player> opponents() {
+        return currentGame.getPlayers();
+    }
+
+    public Hand getHand(){
+        return this.hand;
+    }
+
+    public int getWins() {
+        return this.wins;
+    }
+
+    public void addWin() {
+        wins = wins + 1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Player player = (Player) obj;
+        return Objects.equals(currentGame, player.currentGame) &&
+               Objects.equals(hand, player.hand) &&
+               wins == player.wins;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currentGame, hand, wins);
+    }
+}
